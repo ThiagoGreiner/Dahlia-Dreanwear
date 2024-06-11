b4e910eb03148c53b7fc7d43cec27c00f506eeb2
admin <greinerrodrigues@gmail.com>
2024-06-11 20:58:42 -0300

    Ajuste caminho dos links

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/logolight.png")
+    img.setAttribute("src", "../assets/logolight.png")
   } else {
-    img.setAttribute("src", "./assets/logodark.png")
+    img.setAttribute("src", "../assets/logodark.png")
   }
 }
 */
